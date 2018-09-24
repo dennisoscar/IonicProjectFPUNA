40a5cf134817c4a961c291e5370d730107c78232
DennisOscar <oscaringe91@gmail.com>
2018-09-24 08:46:48 -0400

    Fix de vista regular en el home y agregado de zoom

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -3,19 +3,29 @@ import {NavController} from 'ionic-angular';
 import {DenunciasPage} from "../denuncias/denuncias";
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+  selector: "page-home",
+  templateUrl: "home.html"
 })
 export class HomePage {
-  galleryType ='pinterest';
-  lugaresRecomendados = ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg', '7.jpg', '8.jpg', '9.jpg', '10.jpg', '11.jpg'];
-  constructor(public navCtrl: NavController) {
-
-  }
+  galleryType = "regular";
+  lugaresRecomendados = [
+    "1.jpg",
+    "2.jpg",
+    "3.jpg",
+    "4.jpg",
+    "5.jpg",
+    "6.jpg",
+    "7.jpg",
+    "8.jpg",
+    "9.jpg",
+    "10.jpg",
+    "11.jpg"
+  ];
+  grid = true;
+  constructor(public navCtrl: NavController) {}
 
   goToDenuncias() {
     // NavController es la clase que nos permite navegar entre Pages de nuestra APP
     this.navCtrl.push(DenunciasPage);
   }
-
 }
